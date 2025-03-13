4c82989821774dea6e67a4db8830ce1a0a1d725c
NJUGUNA NJOROGE <engbrian01@gmail.com>
2025-03-13 16:43:28 +0300

    added changes

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+const leastFavoriteCustomer= "kasongo"
+
+function upperCaseCustomerName(){
+    customerName="BOB"
+}
+
+function setBestCustomer(){
+    return bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer='maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+    return (leastFavoriteCustomer = "kasongo");
+  }
+  
